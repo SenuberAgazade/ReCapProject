d8321a8d83dd221aaa9c9fe909d3f36c971d4b10
SenuberAgazade <senuberagazade996@gmail.com>
2021-11-17 10:05:12 +0400

    added  utilities->results

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        public static string MessageAdded = "Added successfully";
+        public static string MessageUpdated = "Updated successfully";
+        public static string MessageDeleted = "Deleted successfully";
+        public static string CarNameInvalid = "Car name must be at least 2 characters";
+        public static string DailyPriceInvalid = "Daily price must be greater than zero";
+        public static string MessageListed = "Listed successfully";
+    }
+}
